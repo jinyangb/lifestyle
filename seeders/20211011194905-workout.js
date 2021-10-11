2692c8458457f621950ecde7c7523e93d3609333
jinyangb <your_email@example.com>
2021-10-11 16:07:00 -0400

    third weight seed

--- seeders/20211011194905-workout.js
+++ seeders/20211011194905-workout.js
@@ -22,6 +22,15 @@ module.exports = {
             'Grip rings or parallel bars with your arms straight. Keeping your chest up, bend your elbows to lower your body as far as your shoulders allow. Press back up powerfully to return to the start.',
           createdAt: new Date(),
           updatedAt: new Date()
+        },
+        {
+          img: 'https://imgur.com/q64p0Vq',
+          type: 'Weight Training',
+          title: 'Incline Dumbbell Press',
+          description:
+            'Lie on an incline bench holding a dumbbell in each hand by your shoulders. Press the weights up until your arms are straight, then lower them back to the start under control.',
+          createdAt: new Date(),
+          updatedAt: new Date()
         }
       ],
       {}
