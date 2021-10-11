ab7cd3d338ddf9a80dac94da2a1d19c841f6a90c
jinyangb <your_email@example.com>
2021-10-11 11:59:55 -0400

    added workout table

---
+++ models/workout.js
@@ -0,0 +1,27 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Workout extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Workout.init(
+    {
+      img: DataTypes.STRING,
+      type: DataTypes.STRING,
+      description: DataTypes.STRING
+    },
+    {
+      sequelize,
+      modelName: 'Workout',
+      tableName: 'workouts'
+    }
+  )
+  return Workout
+}
