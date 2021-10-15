e28f7949ee56e6087711a68d358a079ceff16d76
jinyangb <your_email@example.com>
2021-10-15 12:30:54 -0400

    deploy

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const path = require('path')
 const AppRouter = require('./routes/AppRouter')
 
 const PORT = process.env.PORT || 3001
-const app = express()
 app.use(cors())
 app.use(logger('dev'))
 app.use(bodyParser.json())
