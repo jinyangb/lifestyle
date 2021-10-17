983aac8b63dbf30997c8734564cb55cee4e558fc
jinyangb <your_email@example.com>
2021-10-16 21:38:05 -0400

    services file

--- client/src/globals.js
+++ client/src/globals.js
@@ -1,15 +1,15 @@
 // for heroku API
-import axios from 'axios'
+// import axios from 'axios'
 
-export const BASE_URL =
-  process.env.NODE_ENV === 'production'
-    ? process.env.VUE_APP_API_URL
-    : 'http://localhost:3001/api'
+// export const BASE_URL =
+//   process.env.NODE_ENV === 'production'
+//     ? process.env.VUE_APP_API_URL
+//     : 'http://localhost:3001/api'
 //
 
-axios.create({
-  baseURL:
-    process.env.NODE_ENV === 'production'
-      ? `${window.location.origin}/api`
-      : 'http://localhost:3001/api'
-})
+// axios.create({
+//   baseURL:
+//     process.env.NODE_ENV === 'production'
+//       ? `${window.location.origin}/api`
+//       : 'http://localhost:3001/api'
+// })
